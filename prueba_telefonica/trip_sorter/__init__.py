0546a9d4e85d24e76c89cb553f49314afa3f7906
U01A423F <edominguez@fihoca.com>
2022-05-24 16:56:37 +0200

    fixed an error of __init__.py explanation

--- prueba_telefonica/trip_sorter/__init__.py
+++ prueba_telefonica/trip_sorter/__init__.py
@@ -10,5 +10,4 @@
     https://stackoverflow.com/questions/448271/what-is-init-py-for#:~:text=The%20__init__.py%20file%20makes%20Python%20treat%20directories,the%20submodules%20to%20be%20exported.
 
     https://docs.python.org/3/tutorial/modules.html#packages
-    And it's necessary for the program to work properly.
 """
